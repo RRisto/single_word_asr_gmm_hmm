dae8070a0b4fcc60d8b530a3c22e70c153ced0ef
rristo <ristohinno@gmail.com>
2020-06-07 14:32:18 +0300

    small cleanup

--- predict_google.py
+++ predict_google.py
@@ -1,5 +1,10 @@
 from speech_recognition.HMMSpeechRecog import HMMSpeechRecog
 
-model= HMMSpeechRecog.unpickle('models/google_hmm.pkl')
+model = HMMSpeechRecog.unpickle('models/google_hmm.pkl')
 predicted_labels = model.predict(['data/test_data/output.wav'])
+
 print(f'predicted label {predicted_labels}')
+
+# example if you want to remove original training data to save space
+model.features = None
+model.pickle('models/google_hmm_wo_data.pkl')
