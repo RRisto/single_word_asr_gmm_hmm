50a551043816d2b4e5b2fb40c65e28f0db0f8416
rristo <ristohinno@gmail.com>
2020-06-23 08:07:13 +0300

    google orig experiment

--- predict_google_orig.py
+++ predict_google_orig.py
@@ -11,6 +11,6 @@ original_labels = [str(fl).split('\\')[-2] for fl in test_files]
 report = classification_report(original_labels, predicted_labels)
 conf_mat = model.get_confusion_matrix(original_labels, predicted_labels, list(set(original_labels)))
 
-reports = f'{report}\n {conf_mat}'
+reports = f'{report}\n {conf_mat.to_string()}'
 Path('models/accuracies/accuracy_google_orig_hmm_testdata.txt').write_text(reports)
 print(f'{reports}')
