e9e16239f3b1328924bddf52ceabc3f49512aeef
rristo <ristohinno@gmail.com>
2020-07-05 18:03:22 +0300

    cleanup

--- record_test_audio.py
+++ record_test_audio.py
@@ -1,5 +1,6 @@
 from speech_recogn.record_audio import record_save_audio
 
+#save audio file
 RECORD_SECONDS = 2
 WAVE_OUTPUT_FILENAME = "data/test_data/output.wav"
 record_save_audio(WAVE_OUTPUT_FILENAME, record_seconds=RECORD_SECONDS)
--- run_docker.bat
+++ run_docker.bat
@@ -5,6 +5,7 @@ docker rm single_word_gmmhmm_run
 docker run -it  -d  ^
     -p 0.0.0.0:3332:22  ^
     -p 0.0.0.0:7006:8888  ^
+    --device /dev/snd ^
     -v %APP_PATH%:/opt/single_word_asr_gmm_hmm  ^
     --name single_word_gmmhmm_run  ^
     single_word_gmmhmm
